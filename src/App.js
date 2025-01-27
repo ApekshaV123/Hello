977887fada5022aee68134f2b3ccbf62a77b6eb8
Apeksha Vishwasrao <apekshavishwasrao2002@gmail.com>
2025-01-27 13:24:21 +0530

    initial commit-coffee customization

--- src/App.js
+++ src/App.js
@@ -3,26 +3,37 @@ import './App.css';
 import { use, useState } from 'react';
 
 function App() {
-  const [distance,setDistance]=useState("");
-  const [check,setCheck]=useState("");
-  const transportationModeSelection=()=>{
-    if(distance<3){
-      setCheck("walk");
-    }else if(distance>=3 && distance<=15){
-      setCheck("Bike");
-    }else if(distance>=15){
-      setCheck("car");
+  const [size, setSize] = useState("");
+  const [extraShot, setExtrashot] = useState("");
+  const [check, setCheck] = useState("");
+  const coffeeCustomization = () => {
+    if(size==="small" && extraShot==="order"){
+      setCheck("small with an option for extra shot espresso");
+    } else if(size==="medium" && extraShot==="order"){
+      setCheck("medium with an option for extra shot espresso");
+    } else if(size==="large" && extraShot==="order"){
+      setCheck("large with an option for extra shot espresso");
     }
   }
-  const mode=(event)=>{
-    setDistance(event.target.value);
+  const customerOrder=(event)=>{
+    setSize(event.target.value);
+  }
+
+  const order = (event) => {
+    setExtrashot(event.target.value);
   }
   return (
     <div>
-      <h1>Transportation  Mode Selection</h1>
-      <input type="number" value={distance} onChange={mode}/>
-      <button onClick={transportationModeSelection}>check</button>
-      <p>{check}</p>
+      <h1>Coffee Customization</h1>
+      <select value={size} onChange={customerOrder}>
+        <option value={""}>select</option>
+        <option value={"small"}>Small</option>
+        <option value={"medium"}>Medium</option>
+        <option value={"large"}>Large</option>
+      </select>
+      <input type="text" value={extraShot} onChange={order} />
+      <button onClick={coffeeCustomization}>check</button>
+      <div>{check}</div>
     </div>
   );
 }
