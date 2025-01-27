02a0df55e145a36350c711373bd3d87b5d0ed4f2
Apeksha Vishwasrao <apekshavishwasrao2002@gmail.com>
2025-01-27 10:38:24 +0530

    inital-commit-basic javascript programm

--- src/App.js
+++ src/App.js
@@ -4,38 +4,34 @@ import { use, useState } from 'react';
 
 function App() {
   const [age, setAge] = useState(0);
-  const [check, setCheck] = useState(0);
-
-  const calculatorCheck = () => {
-
-    if (age < 13) {
-      setCheck("child");
-    } else if (age > 13 && age <= 19) {
-      setCheck("Teenager");
-    } else if (age > 20 && age <= 59) {
-      setCheck("Adult");
-    } else {
-      setCheck("Senior");
+  const [price, setPrice] = useState(0);
+  const movieTicketPricingCheck = () => {
+    const todaysDate = new Date();
+    const WeekDays = ["sunday", "Monday", "tuesday", "wednesday", "Thursday", "friday", "satursday"];
+    const todaysDay = todaysDate.getDay();
+    console.log(todaysDay);
+    let tempPrice = 0;
+    if (age < 18) {
+      tempPrice = 8;
+    } else if (age >= 18) {
+      tempPrice = 12;
     }
-
-
-
+    if (WeekDays[todaysDay]==="satursday") {
+      setPrice(tempPrice - 2); 
+    }else{
+      setPrice(tempPrice);
+    }
   }
-
-  const handleAgeChange = (event) => {
-    setAge(event.target.value)
+  const check = (event) => {
+    setAge(event.target.value);
   }
-
-
-
   return (
     <div>
-      <h1>Age Group Categorization</h1>
-      <input type="number" value={age} onChange={handleAgeChange} />
-      <button onClick={calculatorCheck}>calculator</button>
-      <p>{check}</p>
+      <h1>Movie Ticket Pricing</h1>
+      <input type="number" value={age} onChange={check} />
+      <button onClick={movieTicketPricingCheck}>Check</button>
+      <p>{price}</p>
     </div>
-
   );
 }
 
